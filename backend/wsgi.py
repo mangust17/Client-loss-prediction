fc98ce23f3e9426f9e5dc8a311b35dc81ae2f2fb
mangust <matvejlejko5@gmail.com>
2025-05-23 11:32:00 +0300

    2

--- backend/wsgi.py
+++ backend/wsgi.py
@@ -1,4 +1,10 @@
+import sys
+import os
+
+sys.path.insert(0, os.path.abspath(os.path.dirname(__file__)))
+
 from app import app
 
+# Только для локальной отладки (неиспользуется при gunicorn)
 if __name__ == "__main__":
     app.run()
