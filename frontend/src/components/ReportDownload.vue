a9b1d816879bc62e9914a40ee0ef585fc8b80838
mangust <matvejlejko5@gmail.com>
2025-05-25 20:22:29 +0300

    Revert "Поправил стили"
    This reverts commit 11065ff907e6f818a9b191d399afdb9bbc17dcc4.

--- frontend/src/components/ReportDownload.vue
+++ frontend/src/components/ReportDownload.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="report-download">
-    <button class="download-button";@click="downloadReport">Скачать PDF отчет</button>
+    <button @click="downloadReport">Скачать PDF отчет</button>
   </div>
 </template>
 
